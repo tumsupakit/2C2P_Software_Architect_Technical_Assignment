5a797164c49dc311b36fe094e4706c0540311ed4
COMPANY\si <si@manaosoftware.com>
2020-11-14 09:51:42 +0700

    Initial Project

---
+++ 2C2P_Software_Architect_Technical_Assignment/Transaction.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Transaction.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
