2b1b0236f22732316381e5ca580be66592c72f81
COMPANY\si <si@manaosoftware.com>
2020-11-14 10:17:30 +0700

    Initial transaction upload validator service test

---
+++ 2C2P_Software_Architect_Technical_Assignment/Transaction.Test/UnitTest/TransactionUploadValidatorServiceTest.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Transaction.Test
+{
+    [TestClass]
+    public class TransactionUploadValidatorServiceTest
+    {
+        [TestMethod]
+        public void Validate_The_File_Is_Valid_Format_Pass()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void Validate_The_File_Is_Valid_Format_Fail()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void Validate_All_Records_are_valid_Pass()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void Validate_All_Records_are_valid_Fail()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
